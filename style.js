ed46b1685c253459c844391c0c77ba69c435afd4
matthew-pate <mattpate@gmail.com>
2021-01-01 19:50:29 -0600

    updated

--- style.js
+++ style.js
@@ -1,3 +1,12 @@
-function myFunction(x) {
-  x.classList.toggle("change");
+window.onscroll = function () { myFunction() };
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
